04d48219979c1c3e66df19d46ebf033638bc19ed
Grayh4m <61887285+Grayh4m@users.noreply.github.com>
2023-07-14 09:32:03 +0000

    add extra sites and a container for right site content

--- scripts.js
+++ scripts.js
@@ -3,3 +3,8 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 19,
   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+document.getElementById("searchForhelpButton").onclick = function () {
+        window.location = "search_for_help.html";
+        
+    };

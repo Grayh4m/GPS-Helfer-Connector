a0f61cfef28c6f22292d4f10268547918d85e519
Aaron Geiger <aaron.geiger@students.hs-mainz.de>
2023-07-18 11:07:14 +0200

    input now can create markers

--- index_scripts.js
+++ index_scripts.js
@@ -1,3 +1,8 @@
+var receivedArray = JSON.parse(localStorage.getItem("marker"));
+var marker = [];
+if (receivedArray) {
+  marker = receivedArray;
+}
 var map = L.map("map").setView([49.98419, 8.2791], 13);
 L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
   maxZoom: 19,
@@ -6,5 +11,13 @@ L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
 }).addTo(map);
 
 function navigateTo(site) {
+  localStorage.setItem("marker", JSON.stringify(marker));
   window.location = site;
+}
+
+for (var i = 0; i < marker.length; i++) {
+  L.marker([marker[i][0], marker[i][1]])
+    .addTo(map)
+    .bindPopup(marker[i][2])
+    .openPopup();
 }
--- search_for_help_scripts.js
+++ search_for_help_scripts.js
@@ -1,3 +1,24 @@
+var receivedArray = JSON.parse(localStorage.getItem("marker"));
+var marker = [];
+if (receivedArray) {
+  marker = receivedArray;
+}
 function navigateTo(site) {
+  localStorage.setItem("marker", JSON.stringify(marker));
   window.location = site;
+}
+function saveInput() {
+  var input = [
+    document.getElementById("woLatInput").value,
+    document.getElementById("woLongInput").value,
+    document.getElementById("wasInput").value,
+  ];
+  console.log(
+    document.getElementById("woLatInput").value +
+      " " +
+      document.getElementById("woLongInput").value +
+      " " +
+      document.getElementById("wasInput").value
+  );
+  marker.push(input);
 }
